548ebc116f924dcaf4fa8cf7b0732d99fc97b91f
Marian Ciovirta <marian.ciovirta@uipath.com>
2023-10-30 10:07:13 +0200

    Add tmp hook

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Windows;
 using UiPath.CustomProxy.Services;
 using UiPath.CustomProxy.Contracts;
 using System.Windows.Controls;
+using System.Windows.Interop;
+using System;
 
 namespace UiPath.CustomProxy
 {
@@ -23,6 +25,8 @@ namespace UiPath.CustomProxy
             TabControl.SelectedIndex = 0;
 
             WindowState = WindowState.Minimized;
+
+            Loaded += (sender, e) => RegisterHook(); ;
         }
 
         internal MainWindowViewModel ViewModel { get; private set; }
@@ -41,5 +45,23 @@ namespace UiPath.CustomProxy
         public void ExportConfigCommand(object sender, RoutedEventArgs e) => ViewModel.ExportConfig();
 
         private void TextChangedEventHandler(object sender, TextChangedEventArgs args) => ViewModel.UpdateResponses();
+
+        private void RegisterHook()
+        {
+            var helper = new WindowInteropHelper(this);
+            var hwnd = helper.EnsureHandle();
+            var source = HwndSource.FromHwnd(hwnd);
+            source?.AddHook((IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled) =>
+            {
+                ServiceResolver.Get<ILoggingService>().Log($"{msg}");
+#pragma warning disable CS1522 // Empty switch block
+                switch (msg)
+                {
+                }
+#pragma warning restore CS1522 // Empty switch block
+
+                return IntPtr.Zero;
+            });
+        }
     }
 }
